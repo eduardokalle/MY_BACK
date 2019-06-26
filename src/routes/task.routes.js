b09637ab3da278d006b8eea39908d919052846d9
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2019-06-26 17:59:21 -0500

    third commit

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,40 +3,46 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
-router.get('/', async (req , res) => {
-
+router.get('/', async (req, res) => {
     const tasks = await Task.find();
     //console.log(tasks);
     res.json(tasks);
 });
 
-router.get('/:id', async (req , res) => {
-
+router.get('/:id', async (req, res) => {
     const task = await Task.findById(req.params.id);
     //console.log(tasks);
     res.json(task);
 });
 
-router.post('/', async (req , res) =>{
-      const {name,old,address} = req.body;
-      const task = new Task ({name,old,address});
-      await task.save();
-      res.json({status: 'task save '});
-
-
+router.post('/', async (req, res) => {
+    const { name, old, address } = req.body;
+    const task = new Task({ name, old, address });
+    try {
+        await task.save();
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }    
+    res.json({ status: 'task save ', task });
 });
 
-router.put('/:id', async (req , res) =>{
-    const {name,old,address} = req.body;
-    const newTask = {name,old,address};
-    await Task.findByIdAndUpdate(req.params.id, newTask);
-    res.json({status: 'task updated '});
-
+router.put('/:id', async (req, res) => {
+    const { name, old, address } = req.body;
+    const newTask = { name, old, address };
+    try {
+        await Task.findByIdAndUpdate(req.params.id, newTask);
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }    
+    res.json({ status: 'task updated ', task: {
+        ...newTask,
+        _id: req.params.id
+    } });
 });
 
-router.delete('/:id', async (req , res) =>{
+router.delete('/:id', async (req, res) => {
     await Task.findOneAndDelete(req.params.id);
-    res.json({status: 'task deleted '});
+    res.json({ status: 'task deleted ' });
 });
 
 module.exports = router;
